824c5e3e25769a2a27d53f3958f9323d9a302656
LEO <leonardod.trimarchi@gmail.com>
2019-09-16 20:39:38 -0400

    First commit

---
+++ src/Contact.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+
+class Contact extends Component {
+    render() {
+        return (
+            <div>
+                <h2>GOT QUESTIONS?</h2>
+                <p>The easiest thing to do is post on <a href="http://stackoverflow.com">stackoverflow</a></p>
+            </div>
+        );
+    }
+}
+
+export default Contact;
